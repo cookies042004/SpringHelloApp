3b5547ae53cbb4f732f90185cc14845fb0f85fa2
Akhil Puri <ap4723@srmist.edu.in>
2026-03-11 17:01:06 +0530

    [Akhil] Added: UC5: Make REST Call to show Hello Mark Taylor from BridgeLabz using PUT Method

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.example.springboot.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.springboot.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @PostMapping("/hello/post")
-    public String sayHello(@RequestBody UserDTO user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    @PutMapping("/hello/{firstName}")
+    public String sayHello(
+            @PathVariable String firstName,
+            @RequestParam String lastName) {
+
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }

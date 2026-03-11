f8f5476bdd845f29c85c3d0fda24b43f9a2b3261
Akhil Puri <ap4723@srmist.edu.in>
2026-03-11 16:38:57 +0530

    [Akhil] Added: UC2: Use GET Request Method and pass name as query parameter

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello, " + name + " from BridgeLabz";
     }
 }
